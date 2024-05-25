8b286cf95c601f1ddca4fe741beffcefed9881ed
Jeff Goes <goes.jeffjulian@gmail.com>
2024-05-25 14:19:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { employeesData } from "./data.js";
+const app = express();
+
+// Getting data
+app.get("/employees", (req, res) => {
+  res.json(employeesData);
+});
+
+app.listen(4000, () => {
+  console.log("Server started on port 4000");
+});
